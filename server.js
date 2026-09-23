a5123e4407f2a4028c8bfba3ea04f2e07671a8ad
Hemachro <124392305+HeMaChRo@users.noreply.github.com>
2026-09-23 15:06:46 -0600

    Actualizar version 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
     estado: "Activo",
-    version: "1.0.1",
+    version: "1.0.2",
     mensaje: "Microservicio desplegado automáticamente en la Nube",
     timestamp: new Date().toISOString()
   }));
